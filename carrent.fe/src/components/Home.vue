4fbfeaa4f3ccb3fa8de73459a6da1c110d9c91dd
Miroslav Pakanec <miropakanec@gmail.com>
2017-11-18 11:18:45 +0100

    added parallax responsive to all devices

--- carrent.fe/src/components/Home.vue
+++ carrent.fe/src/components/Home.vue
@@ -1,3 +1,97 @@
 <template>
-  
+    <div class="container">
+        <header class="background">
+            <div class="car"
+                 v-bind:style="getCarStyle"></div>
+            <div class="title"
+                 v-bind:style="getTitleStyle"></div>
+        </header>
+    </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      scroll: 0
+    };
+  },
+  computed: {
+    getTitleStyle() {
+      return { transform: "translate(0px, " + this.scroll / 20 + "%)" };
+    },
+    getCarStyle() {
+      var num = this.scroll;
+      num = num.map(0, 1500, 1, 2);
+
+      var scale = "scale(" + num + "," + num + ")";
+      var translation = "translate(0px, " + this.scroll / 40 + "%)";
+      return { transform: scale + " " + translation }
+    }
+  },
+  methods: {
+    handleScroll: function(event) {
+      var doc = document.documentElement;
+      var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+
+      this.scroll = top;
+    }
+  },
+  created: function() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed: function() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+};
+
+Number.prototype.map = function(in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
+</script>
+
+
+<style>
+.container {
+  height: 3000px;
+  width: 100%;
+  max-width: 1500px;
+  padding: 0;
+  margin: 0;
+}
+
+/* paralax */
+.background {
+  position: relative;
+  height: 50vw;
+  width: 100%;
+  overflow: hidden;
+
+  background-image: url("./../../static/parallax/background.jpg");
+  background-size:contain;
+  background-position: top center;
+  background-attachment: fixed;
+}
+
+.car {
+  width: 100%;
+  height:  800px;
+  background-image: url("./../../static/parallax/car.png");
+  background-position: left top;
+  background-size: contain;
+
+  position: absolute;
+  top: -25%;
+}
+
+.title {
+  height:  800px;
+  width: 100%;
+  background-image: url("./../../static/parallax/auto.png");
+  background-position: top center;
+  background-size: contain;
+
+  position: absolute;
+  top: -45%;
+}
+</style>
--- carrent.fe/src/components/Navbar.vue
+++ carrent.fe/src/components/Navbar.vue
@@ -1,33 +1,82 @@
 <template>
   <div>
-      <v-toolbar color="white" flat class="border-bottom">
-        <v-toolbar-side-icon class="hidden-md-and-up standard-color"></v-toolbar-side-icon>
-        <v-layout justify-center fill-height row wrap class="hidden-sm-and-down">
-            <v-spacer></v-spacer>
-            <v-flex xs3 md2 text-xs-center 
-                v-for="item in menuItems" 
+    <v-layout>
+      <v-navigation-drawer temporary
+                           v-model="drawer"
+                           light
+                           absolute>
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://deltafonts.com/wp-content/uploads/Levis-Logo.png" />
+            </v-list-tile-avatar>
+          </v-list-tile>
+        </v-list>
+        <v-list class="pt-0"
+                dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in menuItems"
+                       :key="item.title"
+                       @click="''" class="menu-item">
+            <v-list-tile-action>
+              <v-icon class="standard-color">{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="standard-color">{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+    <v-toolbar color="white"
+               flat
+               class="border-bottom">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"
+                           class="hidden-md-and-up standard-color"></v-toolbar-side-icon>
+      <v-layout justify-center
+                fill-height
+                row
+                wrap
+                class="hidden-sm-and-down">
+        <v-spacer></v-spacer>
+        <v-flex xs3
+                md2
+                text-xs-center
+                v-for="item in menuItems"
                 :key="item.title">
-                <v-btn block flat class="fill ver-align-center menu-item standard-color">
-                    {{ item.title }}
-                </v-btn>
-            </v-flex>     
-                        <v-spacer></v-spacer>
-                                                                 
-        </v-layout>
-      </v-toolbar>
+          <v-btn block
+                 flat
+                 class="fill ver-align-center menu-item standard-color">
+            <v-layout row
+                      wrap>
+              <v-flex xs12>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-flex>
+              <v-flex xs12>
+                <span>{{ item.title }}</span>
+              </v-flex>
+            </v-layout>
+          </v-btn>
+        </v-flex>
+        <v-spacer></v-spacer>
+      </v-layout>
+    </v-toolbar>
   </div>
 </template>
 
 <script>
+import NavbarDrawer from "./../components/Navbar.vue";
+
 export default {
   data() {
     return {
       menuItems: [
-        { title: "ponuka", icon: "home" },
-        { title: "kto sme", icon: "home" },
-        { title: "podmienky", icon: "home" },
-        { title: "kontakt", icon: "home" }
-      ]
+        { title: "Ponuka", icon: "directions_car" },
+        { title: "Kto sme", icon: "fingerprint" },
+        { title: "Podmienky", icon: "warning" },
+        { title: "Kontakt", icon: "phone" }
+      ],
+      drawer: null
     };
   }
 };
@@ -38,6 +87,7 @@ export default {
 .menu-item {
   font-family: Intro;
   font-size: 1.3em;
+  
 }
 
 .fill {
@@ -55,3 +105,4 @@ export default {
   color: rgb(197, 18, 48) !important;
 }
 </style>
+
